11a52116cbca9b7b2d0abda71a1ea10ff0aa2111
aruna <aruna@gogosoon.com>
2024-01-06 18:49:09 +0530

    [Aruna] updates create user

--- src/routes/user_route.ts
+++ src/routes/user_route.ts
@@ -1,22 +1,40 @@
 import express from 'express';
 import { UserModel } from '../model/user_model';
+import { validateAdmin } from '../middleware/auth.middleware';
+import { getHashOfPassword } from '../helper/passwordHelper';
 
-const route = express.Router()
+const router = express.Router()
 
+router.get("/create", validateAdmin, async (req, res) => {
 
-
-route.get("/create", (req, res) => {
-
-    UserModel.create({
-        firstName: "JKohn",
-        lastName: "Bob",
-        email: "john@gmail.com"
-    });
-
-    res.json({
-        "ke": "value"
-    })
+    const { firstName, lastName, email, password, isAdmin } = req.body;
+    if (!firstName || !lastName || !email || !password) {
+        return res.status(400).json({ error: 'All fields (firstName, lastName, email, password) are required.' });
+      }
+    try {
+        // Check if a user with the provided email already exists
+        const existingUser = await UserModel.findOne({ where: { email } });
+    
+        if (existingUser) {
+          return res.status(400).json({ error: 'User with this email already exists' });
+        }
+    
+        // Create a new user record in the database
+        const newUser = await UserModel.create({
+          firstName,
+          lastName,
+          email,
+          password:getHashOfPassword(password),
+          isAdmin,
+        });
+    
+        console.log('User created successfully:', newUser.toJSON());
+        res.status(201).json({ message: 'User created successfully', user: newUser.toJSON() });
+      } catch (error) {
+        console.error('Error creating user:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
 })
 
 
-export default route
+export default router
