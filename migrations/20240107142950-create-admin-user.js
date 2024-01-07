f56788345cb6f9366e872f21cfa0650ac72f3fe7
aruna <aruna@gogosoon.com>
2024-01-07 20:04:18 +0530

    [Aruna] adds migration to create admin user

---
+++ migrations/20240107142950-create-admin-user.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('users', [{
+      firstName: 'Admin',
+      lastName: 'User',
+      email: 'admin@riktam.com',
+      password: '$2b$10$6gbToSC9rplY2t.SjOpQUe13j2v1ZM9vZnO5BTn3c3drVHuh4LLMO', 
+      isAdmin: true,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('users', { email: 'admin@example.com' }, {});
+  }
+};
