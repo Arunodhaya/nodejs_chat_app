b26dc40af7ff2ab2b001977d3b435e6b971294a7
aruna <aruna@gogosoon.com>
2024-01-07 23:33:04 +0530

    [Aruna] updates Userflow testcases

--- src/routes/auth_route.test.ts
+++ src/routes/auth_route.test.ts
@@ -1,7 +1,24 @@
 import { beforeAll, describe, expect, test } from '@jest/globals';
 import { request_helper } from '../tests/request_helper';
+const { v4: uuidv4 } = require('uuid');
 
-describe('Login Route', () => {
+let admin_token = "";
+
+beforeAll(async () => {
+    // Log in as admin to get the authentication token
+    const validUser = {
+        email: 'admin@riktam.com',
+        password: 'admin@123',
+    };
+    const loginResponse = await request_helper.request('POST', '/auth/login', validUser, {});
+    expect(loginResponse.status).toBe(200);
+    expect(loginResponse.data.token).toBeDefined();
+    admin_token = loginResponse.data.token;
+});
+
+
+
+describe('Test users flow', () => {
     test('Invalid email or password', async () => {
         const invalidUser = {
             email: 'test@example.com',
@@ -17,64 +34,78 @@ describe('Login Route', () => {
             expect(loginResponse.data.error).toBe('Invalid email or password.');
         }
     });
-    let admin_token = "";
 
-    beforeAll(async () => {
-        // Log in as admin to get the authentication token
-        const validUser = {
-            email: 'admin@riktam.com',
-            password: 'admin@123',
-        };
-        const loginResponse = await request_helper.request('POST', '/auth/login', validUser, {});
-        expect(loginResponse.status).toBe(200);
-        expect(loginResponse.data.token).toBeDefined();
-        admin_token = loginResponse.data.token;
-    });
+    // user 1
+    let createdUser1Id = null;
+    let createdUser1Email = null;
+    let createdUser1Token = null;
 
-    let createdUserId;
+    // user 2
+    let createdUser2Id = null;
 
-
-    test('Create Admin User', async () => {
-        const { v4: uuidv4 } = require('uuid');
-
+    test('Create User 1 and User 2', async () => {
         const uniqueEmail = `john.doe.${uuidv4()}@example.com`; // Generating a unique email using UUID
-
-        const newAdminUser = {
+        let newUser = {
             firstName: 'John',
             lastName: 'Doe',
             email: uniqueEmail,
             password: 'securepassword',
             isAdmin: false,
         };
-
+        createdUser1Email = uniqueEmail
         try {
-            const createResponse = await request_helper.request('POST', '/users/create', newAdminUser, {
+            const createResponse = await request_helper.request('POST', '/users/create', newUser, {
                 Authorization: `Bearer ${admin_token}`,
             });
-
             expect(createResponse.status).toBe(201);
             expect(createResponse.data.message).toBe('User created successfully');
             expect(createResponse.data.user).toBeDefined();
-            createdUserId = createResponse.data.user.id;
+            createdUser1Id = createResponse.data.user.id;
+
+            let newUser2 = {
+                firstName: 'Bob',
+                lastName: 'Doe',
+                email: `Bob.doe.${uuidv4()}@example.com`,
+                password: 'securepassword',
+                isAdmin: false,
+            };
+
+            // creating user 2
+            const createUser2Response = await request_helper.request('POST', '/users/create', newUser2, {
+                Authorization: `Bearer ${admin_token}`,
+            });
+            createdUser2Id = createUser2Response.data.user.id;
+
+            expect(createUser2Response.data.user).toBeDefined();
+
         } catch (err) {
             console.log(err.response.data);
         }
     });
 
-    test('Edit Admin User', async () => {
-        const { v4: uuidv4 } = require('uuid');
+    test("Login as newly created user", async () => {
+        // Log in as admin to get the authentication token
+        const validUser = {
+            email: createdUser1Email,
+            password: 'securepassword',
+        };
+        const loginResponse = await request_helper.request('POST', '/auth/login', validUser, {});
+        expect(loginResponse.status).toBe(200);
+        expect(loginResponse.data.token).toBeDefined();
+        createdUser1Token = loginResponse.data.token;
+    })
 
-        const uniqueEmail = `john.doe.${uuidv4()}@example.com`; // Generating a unique email using UUID
+    test('Edit User the created user with Admin Token', async () => {
 
-        const updatedAdminUser = {
+        const updateUser = {
             firstName: 'UpdatedJohn',
             lastName: 'UpdatedDoe',
-            email: uniqueEmail,
+            email: createdUser1Email,
             isAdmin: false,
             password: 'newsecurepassword',
         };
 
-        const editResponse = await await request_helper.request('PUT', `/users/edit/${createdUserId}`, updatedAdminUser, {
+        const editResponse = await request_helper.request('PUT', `/users/edit/${createdUser1Id}`, updateUser, {
             Authorization: `Bearer ${admin_token}`,
         });
 
@@ -83,7 +114,7 @@ describe('Login Route', () => {
         expect(editResponse.data.user).toBeDefined();
         expect(editResponse.data.user.firstName).toBe('UpdatedJohn');
         expect(editResponse.data.user.lastName).toBe('UpdatedDoe');
-        expect(editResponse.data.user.email).toBe(uniqueEmail);
+        expect(editResponse.data.user.email).toBe(createdUser1Email);
         expect(editResponse.data.user.isAdmin).toBe(false);
     });
 
