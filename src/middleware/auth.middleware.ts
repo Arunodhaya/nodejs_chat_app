61fdefdfeab4479376357545b2e1e1765b14bfd3
aruna <aruna@gogosoon.com>
2024-01-07 11:25:55 +0530

    [Aruna] adds validateUser middleware

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -13,21 +13,34 @@ export const generateToken = (user: UserModel ): string => {
   return jwt.sign({ user }, SECRET_KEY);
 }
 
+const verifyToken = (token: string) => {
+  return jwt.verify(token, SECRET_KEY) as JwtPayload 
+};
+
+const validateToken = (req: any, res: Response, next: NextFunction, isAdminValidation: boolean) => {
+  const token = extractAuthToken(req);
+  if (!token) {
+    return res.status(401).json({ error: 'Unauthorized. Please Login.' });
+  }
+
+  try {
+    const decoded = verifyToken(token);
+    if (isAdminValidation && !decoded.user.isAdmin) {
+      return res.status(403).json({ error: 'Forbidden. Admin access required.' });
+    }
+
+    req.user = decoded.user;
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(401).json({ error: 'Unauthorized. Invalid token.' });
+  }
+};
+
 export const validateAdmin = (req, res: Response, next: NextFunction) => {
-    const token = extractAuthToken(req);
-    if (!token) {
-      return res.status(401).json({ error: 'Unauthorized. Please Login.' });
-    }
-    try {
-      const decoded = jwt.verify(token,SECRET_KEY) as JwtPayload 
-      if (!decoded.user.isAdmin) {
-        return res.status(403).json({ error: 'Forbidden. Admin access required.' });
-      }
-  
-      req.user = decoded.user;
-      next();
-    } catch (error) {
-      console.error(error);
-      res.status(401).json({ error: 'Unauthorized. Invalid token.' });
-    }
+  validateToken(req, res, next, true);
   };
+
+export const validateUser = (req: any, res: Response, next: NextFunction) => {
+  validateToken(req, res, next, false);
+};
